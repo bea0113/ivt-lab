5bad161a233550f972daecd075498a9673cce2fb
Bea0113 <sudi.beata@gmail.com>
2020-05-07 00:26:31 +0200

    added dependency injection, and unittestin gwith mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,31 +9,45 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockTorpedoStorePrimary;
+  private TorpedoStore mockTorpedoStoreSecondary;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockTorpedoStorePrimary = mock(TorpedoStore.class);
+    mockTorpedoStoreSecondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTorpedoStorePrimary,mockTorpedoStoreSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockTorpedoStorePrimary.isEmpty()).thenReturn(true);
+    when(mockTorpedoStoreSecondary.isEmpty()).thenReturn(false);
+    when(mockTorpedoStoreSecondary.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
+    verify(mockTorpedoStorePrimary, times(0)).fire(1);
+    verify(mockTorpedoStoreSecondary, times(1)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(mockTorpedoStorePrimary.isEmpty()).thenReturn(false);
+    when(mockTorpedoStoreSecondary.isEmpty()).thenReturn(false);
+    when(mockTorpedoStorePrimary.fire(1)).thenReturn(true);
+    when(mockTorpedoStoreSecondary.fire(1)).thenReturn(true);
+    
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
+    verify(mockTorpedoStorePrimary, times(1)).fire(1);
+    verify(mockTorpedoStoreSecondary, times(1)).fire(1);
     assertEquals(true, result);
   }
 
